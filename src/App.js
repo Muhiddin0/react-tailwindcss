9285e3083339262c4c4714ab477dc1ffc4e4cd3d
muhiddin0 <kabraliev2005@gmail.com>
2024-03-16 13:50:26 +0500

    first alredy commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./assets/tailwid.css";
+import Header from "./components/common/logo";
 
-function App() {
+import { HiOutlineMailOpen } from "react-icons/hi";
+
+function MyApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <section>
+        <div className="container flex-col lg:flex-row min-h-[100vh] flex items-center justify-evenly lg:justify-center">
+          <div className="w-[100%] lg:w-[50%] text-center lg:text-start">
+            <h1 className="font-bold text-3xl">Assalomu Alekum 👋</h1>
+            <p>
+              Bring all of your content together and get a Homepage that
+              automatically updates whenever you create anywhere online.
+            </p>
+          </div>
+
+          <div className="w-[100%] lg:w-[50%] flex items-center justify-center">
+            <div className="w-[400px] shadow-lg rounded-md overflow-hidden bg-white">
+              <div className="flex items-center text-white justify-center bg-purple-600 p-3">
+                <HiOutlineMailOpen size={35} />
+              </div>
+
+              <div className="px-3 py-5">
+                <p className="text-slate-500 text-center">
+                  and get a Homepage thatand get a Homepage that
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
-export default App;
+export default MyApp;
---
+++ src/components/common/logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <div className="font-semibold text-lg text-purple-600">Loog</div>
+      </div>
+    </header>
+  );
+}
